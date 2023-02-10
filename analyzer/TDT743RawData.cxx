0453aca3c80f6d2b730dbac4ac0a290c4b474f07
Félix Cormier <fcormier@Felixs-MacBook-Pro.local>
2023-02-10 14:04:43 +0900

    First try to update TDTRawData to dt5720

--- analyzer/TDT743RawData.cxx
+++ analyzer/TDT743RawData.cxx
@@ -19,42 +19,48 @@ TDT743RawData::TDT743RawData(int bklen, int bktype, const char* name, void *pdat
   fTriggerTime = 0;
   // Do some sanity checking.  
   // Make sure first word has right identifier
+  //I think this can be re-used for DT5720, but I don't get why printout uses [0]
   if( (GetData32()[2] & 0xf0000000) != 0xa0000000) 
     std::cerr << "First word has wrong identifier; first word = 0x" 
 	      << std::hex << GetData32()[0] << std::dec << std::endl;
   
   int counter = 6;
   
-  int number_available_groups = 0;
-  for(int ch = 0; ch < 16; ch++){	
+  //Switching from groups to channels for DT5720
+  int number_available_channels = 0;
+  for(int ch = 0; ch < 4; ch++){	
   	//std::cout <<  	(1<<ch)  <<  " " << GetChMask() << " " << ((1<<ch) & GetChMask()) << std::endl;
+	//Bit-wise add!
     if((1<<ch) & GetChMask()){
-      number_available_groups++;
+      number_available_channels++;
     }
   }
   
-  int nwords_per_group = (GetEventSize() - 4)/number_available_groups;
-  if(0)std::cout << "Words per group : " << nwords_per_group << " " 
-	    << number_available_groups << std::endl;
+  int nwords_per_channel = (GetEventSize() - 4)/number_available_channels;
+  if(0)std::cout << "Words per group : " << nwords_per_channel << " " 
+	    << number_available_channels << std::endl;
 
   // Loop over groups
-  for(int gr = 0; gr < number_available_groups; gr++){
-    if(gr == 0) fTriggerTime = 0;
+  //For DT5720: loops over channels
+  std::cout << "Available channels:" << number_available_channels << std::endl;
+  std::cout << "nwords per channels:" << nwords_per_channel << std::endl;
+  for(int channel = 0; channel < number_available_channels; channel++){
+    if(channel == 0) fTriggerTime = 0;
     if((GetData32()[counter] & 0xff000000) != 0xaa000000){
       std::cout << "Bad first word for group: 0x" << std::hex << GetData32()[counter] << std::dec << std::endl;
     }
 
-    if((1<<gr) & GetChMask()){
+    if((1<<channel) & GetChMask()){
       
-      std::vector<uint32_t> Samples0;
-      std::vector<uint32_t> Samples1;
-      int time0=0, time1=0, hit0=0, hit1=0, freq=0;
+      std::vector<uint32_t> Samples;
       
       //std::cout << "++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
       //std::cout << nwords_per_group << " " << number_available_groups << std::endl;
       
-      for(int i = 0; i < nwords_per_group; i++){
+	//Don't think extra info is in DT5720
+      for(int i = 0; i < nwords_per_channel; i++){
 	// Get extra info
+	/*
 	if(i == 1) hit0  += (GetData32()[counter] & 0xff000000) >> 24;
 	if(i == 2) hit0  += (GetData32()[counter] & 0xff000000) >> 16;
 	if(i == 3) time0 += (GetData32()[counter] & 0xff000000) >> 24;
@@ -64,7 +70,7 @@ TDT743RawData::TDT743RawData(int bklen, int bktype, const char* name, void *pdat
 	if(i == 7) time1 += (GetData32()[counter] & 0xff000000) >> 24;
 	if(i == 8) time1 += (GetData32()[counter] & 0xff000000) >> 16;
 	if(i == 9) freq  = (GetData32()[counter] & 0xff000000) >> 24;
-    if(gr == 0){
+    if(channel == 0){
 		
 		if(i == 13)  {
 			fTriggerTime += (uint64_t)(GetData32()[counter] & 0xff000000) >> 24;
@@ -75,11 +81,12 @@ TDT743RawData::TDT743RawData(int bklen, int bktype, const char* name, void *pdat
 		if(i == 15)  fTriggerTime += (uint64_t)(GetData32()[counter] & 0xff000000) >> 8;
 		if(i == 16)  fTriggerTime += (uint64_t)(GetData32()[counter] & 0xff000000) ;
 		if(i == 17)  fTriggerTime += (uint64_t)(GetData32()[counter] & 0xff000000) << 8;
-		/*if(i == 17) {
+		if(i == 17) {
 			std::bitset<40> c(fTriggerTime);
 			std::cout << c << " " << ((uint64_t)(GetData32()[counter] & 0xff000000) << 8) <<  " " << GetEventCounter() << std::endl;
-		}*/
+		}
 	}
+	*/
 	
     /*if(gr == 0){
 		
@@ -98,12 +105,12 @@ TDT743RawData::TDT743RawData(int bklen, int bktype, const char* name, void *pdat
 		}
 	}*/
 	// Get Samples (skip 17th sample)
-	if(i%17 != 0){
+	//Don't skip for DT5720
+	//Changed format for DT5720
 	  uint32_t sample = (GetData32()[counter] & 0xfff);
-	  Samples0.push_back(sample);
-	  sample = (GetData32()[counter] & 0xfff000) >> 12;
-	  Samples1.push_back(sample);				
-	}
+	  Samples.push_back(sample);
+	  sample = (GetData32()[counter] & 0xfff0000) >> 16;
+	  Samples.push_back(sample);				
 	/*else{
 	  std::cout << i << " " << (GetData32()[counter] & 0xfff) << " " << ((GetData32()[counter] & 0xfff000) >> 12) << std::endl;
 	}
@@ -114,16 +121,10 @@ TDT743RawData::TDT743RawData(int bklen, int bktype, const char* name, void *pdat
 	// increment pointer
 	counter++;
       }
-      RawChannelMeasurement meas0 = RawChannelMeasurement(gr*2);
-      meas0.AddSamples(Samples0);
-      meas0.SetFrequency(freq);
-      meas0.SetHitCounter(hit0);
-      meas0.SetTimeCounter(time0);
-      fMeasurements.push_back(meas0);
+	  
+      RawChannelMeasurement meas = RawChannelMeasurement(ch*2);
+      meas.AddSamples(Samples);
 
-      RawChannelMeasurement meas1 = RawChannelMeasurement(gr*2+1);
-      meas1.AddSamples(Samples1);
-      fMeasurements.push_back(meas1);
       
       if(0)std::cout << freq << " " << hit0 << " " << hit1 
 		     << " " << time0 << " " << time1 << std::endl;
